20298418325d7452a6d19db5ada0ed90a160e536
Cosmin Vulpe <cosmin98vulpe@gmail.com>
2022-04-11 12:04:59 +0300

    Add design page: navBar, footer

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -33,10 +33,15 @@ public class Initializer implements ServletContextListener {
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(tablet);
+        ProductCategory smartPhones = new ProductCategory("SmartPhones", "Hardware", "A smartphone is a handheld electronic device that provides a connection to a cellular network and the internet.");
+        productCategoryDataStore.add(smartPhones);
+
 
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", new BigDecimal("479"), "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", new BigDecimal("89"), "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+        productDataStore.add(new Product("Iphone 12", new BigDecimal("100"), "USD", "Tim Cook latest iphone.", smartPhones, amazon));
     }
+
 }
